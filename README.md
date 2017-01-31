c41d299be38c82086a25047512769a09a5de8c71
u10e10 <uplus.e10@gmail.com>
2017-01-31 18:40:16 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Otaku Discrimination Sever
+
+
+## Setup
+
+```bash
+pip3 install -r require.txt
+```
+
+## Run
+
+```bash
+python3 ./server.py
+```
+
+## Sample Usage
+
+```bash
+curl 'http://0.0.0.0:5000/post' -X POST -F 'img=@image.jpg' 
+```
---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+import os
+
+app = Flask(__name__)
+form_name = 'img'
+
+# ブラウザ用
+@app.route('/')
+def index():
+    return render_template('index.html', name=form_name)
+
+# imgフォームに画像をセットしてPOST
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    if request.method != 'POST':
+        return "error: POST only\n"
+    if form_name not in request.files:
+        return "error: Set image to 'img' form\n"
+
+    img = request.files[form_name]
+    if not img.filename:
+        return "error: Set image to 'img' form\n"
+
+    path = os.path.join('images', secure_filename(img.filename))
+    img.save(path)
+    return 'yes' if is_otaku(path) else 'no'
+
+# TODO 判別機
+def is_otaku(path):
+    return True
+
+
+if __name__ == '__main__':
+    app.debug = os.getenv('OTAKU_DEBUG', False)
+    app.run(host='0.0.0.0')
